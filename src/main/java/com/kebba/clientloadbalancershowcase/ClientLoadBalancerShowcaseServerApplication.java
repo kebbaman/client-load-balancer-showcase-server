97dd878f3c11a19f679cb7a17a0adc44d2cf4bd1
kebba <kebbaman@gmail.com>
2022-09-10 14:53:07 +0200

    First commit

---
+++ src/main/java/com/kebba/clientloadbalancershowcase/ClientLoadBalancerShowcaseServerApplication.java
@@ -0,0 +1,13 @@
+package com.kebba.clientloadbalancershowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientLoadBalancerShowcaseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientLoadBalancerShowcaseServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kebba/clientloadbalancershowcase/ClientLoadBalancerShowcaseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kebba.clientloadbalancershowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientLoadBalancerShowcaseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
